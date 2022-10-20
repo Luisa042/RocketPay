35989b88ab860cb5644126c4f5a7f8f27542142d
Luisa <luisa.santos042@gmail.com>
2022-10-20 18:09:46 +0000

    implement function to change card appearance

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-import "./css/index.css"
+import './css/index.css'
+
+const cardBgColor1 = document.querySelector(
+  '.cc-bg svg > g g:nth-child(1) path'
+)
+const cardBgColor2 = document.querySelector(
+  '.cc-bg svg > g g:nth-child(2) path'
+)
+const cardLogo = document.querySelector('.cc-logo span:nth-child(2) img')
+
+function setCardType(type) {
+  const colors = {
+    default: ['black', 'gray'],
+    visa: ['#436D99', '#2D57F2'],
+    mastercard: ['#C69347', '#DF6F29'],
+  }
+
+  cardBgColor1.setAttribute('fill', colors[type][0])
+  cardBgColor2.setAttribute('fill', colors[type][1])
+  cardLogo.setAttribute('src', `cc-${type}.svg`)
+}
+globalThis.setCardType = setCardType
